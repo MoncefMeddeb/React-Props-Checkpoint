8a430653f4c71b279e47b663dda1fb647e985a79
MoncefMeddeb <meddebmoncef4@gmail.com>
2024-01-19 05:00:05 +0100

    first commit

---
+++ props/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PlayersList from './PlayerList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FIFA Player Cards</h1>
+      <PlayersList />
+    </div>
+  );
+}
+
+export default App;
---
+++ props/src/players.js
@@ -0,0 +1,36 @@
+const players = [
+{
+    name: "Cristiano Ronaldo dos Santos Aveiro",
+    team: "Al-Nassr FC",
+    nationality: "Portuguese",
+    jerseyNumber: 7,
+    age: 39,
+    imageUrl: "https://1.bp.blogspot.com/-okTnKkYND9o/TiV4mn5iq3I/AAAAAAAAEdc/4zdDA4nIYqk/s1600/ronaldo3.jpg",
+},
+{
+    name: "Lionel Messi",
+    team: "Inter Miami CF",
+    nationality: "Argentina",
+    jerseyNumber: 10,
+    age: 36,
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/c/c8/Lionel_Messi_WC2022.jpg",
+    },
+    {
+    name: "Kylian Mbappé",
+    team: "Paris Saint-Germain",
+    nationality: "French",
+    jerseyNumber: 7,
+    age: 25,
+    imageUrl: "https://media.gqmagazine.fr/photos/5b990a7be1cf6200114bf6dc/1:1/w_2048%2cc_limit/la_magnifique_blague_de_jimmy_fallon_sur_mbapp___6096.jpeg",
+    },
+    {
+    name: "Vinícius José Paixão de Oliveira Júnior",
+    team: "Real Madrid",
+    nationality: "BRAAAAAAAAAAAAAAAAAZZZZZZZZZZZZZZIIIIIIIIIIIIIW",
+    jerseyNumber: 7,
+    age: 23,
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/f/f3/Vinicius_Jr_2021.jpg",
+    },
+];
+        
+export default players;
